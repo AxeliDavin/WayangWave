d7aa09d0e6d5ef69bdba226febebdf82d270ca2e
AxeliDavin <axelius.davin@gmail.com>
2023-11-10 13:39:07 +0700

    Tidak tahu

---
+++ scr/adt.h
@@ -0,0 +1,7 @@
+#include "scr/ADT/list.h"
+#include "scr/ADT/map.h"
+#include "scr/ADT/mesinkarakter.h"
+#include "scr/ADT/mesinkata.h"
+#include "scr/ADT/queue.h"
+#include "scr/ADT/set.h"
+#include "scr/ADT/stack.h"
---
+++ scr/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "console.c"
+
+int main(){
+    
+}
